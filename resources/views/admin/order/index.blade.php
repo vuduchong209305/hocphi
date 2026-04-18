7285d18943983814f365ba19bcdbee936de2b49c
vuduchong209305 <vuduchong209305@gmail.com>
2026-04-18 21:19:57 +0700

    responsive table mb

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -75,79 +75,82 @@
 
         {{ !empty($orders) ? vdh_paginate($orders, $orders->total()) : null }}
 
-        <table class="table table-hover table-vcenter table-bordered table-striped" id="datatable" width="100%">
-            <thead>
-                <tr>
-                    <th width="10%">Mã thanh toán</th>
-                    <th width="35%">Khóa học</th>
-                    <th width="25%">Thông tin</th>
-                    <th width="20%">Giấy tờ</th>
-                    <th width="10%">#</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if(isset($orders) && count($orders) > 0) 
-                    @foreach($orders as $key => $val)
-                        <tr>
-                            <td>
-                                <p class="mb-0">{{ $val->code ?? null }}</p>
+        <div class="table-responsive">
+            <table class="table table-hover table-vcenter table-bordered table-striped" width="100%">
+                <thead>
+                    <tr>
+                        <th width="10%">Mã thanh toán</th>
+                        <th width="35%">Khóa học</th>
+                        <th width="25%">Thông tin</th>
+                        <th width="20%">Giấy tờ</th>
+                        <th width="10%">#</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if(isset($orders) && count($orders) > 0) 
+                        @foreach($orders as $key => $val)
+                            <tr>
+                                <td>
+                                    <p class="mb-0">{{ $val->code ?? null }}</p>
 
-                                <p class="mb-2">
-                                    {!! !empty($val->paid_at) ? "<span class='badge bg-success-subtle text-success fw-semibold rounded-pill'>{$val->paid_at}</span>" : '<span class="badge bg-warning-subtle text-warning fw-semibold rounded-pill">Chưa thanh toán</span>' !!}
-                                </p>
+                                    <p class="mb-2">
+                                        {!! !empty($val->paid_at) ? "<span class='badge bg-success-subtle text-success fw-semibold rounded-pill'>{$val->paid_at}</span>" : '<span class="badge bg-warning-subtle text-warning fw-semibold rounded-pill">Chưa thanh toán</span>' !!}
+                                    </p>
 
-                                <p class="mb-2">
-                                    <a href="#" data-type="select" class="assigned_to" data-name="assigned_to" data-pk="{{ $val->id ?? null }}" data-url="{{ route('order.update') }}" data-title="Người phụ trách" data-value="{{ $val->assigned_to }}">{{ $val->assignedTo->fullname ?? 'Người phụ trách' }}</a>
-                                </p>
+                                    <p class="mb-2">
+                                        <a href="#" data-type="select" class="assigned_to" data-name="assigned_to" data-pk="{{ $val->id ?? null }}" data-url="{{ route('order.update') }}" data-title="Người phụ trách" data-value="{{ $val->assigned_to }}">{{ $val->assignedTo->fullname ?? 'Người phụ trách' }}</a>
+                                    </p>
 
-                                <p class="mb-0">
-                                    <a href="#" data-type="select" class="status" data-name="status_id" data-pk="{{ $val->id ?? null }}" data-url="{{ route('order.update') }}" data-title="Trạng thái" data-value="{{ $val->status_id }}">{{ $val->status->name ?? 'Trạng thái' }}</a>
-                                </p>
-                            </td>
-                            <td>
-                                <p class="mb-0">Tên: <b>{{ $val->course->title ?? null }}</b></p>
-                                <p class="mb-0">Lớp: {{ $val->class_code ?? null }}</p>
-                                <p class="mb-0">Khai giảng: {{ $val->start_date ?? null }}</p>
-                                <p class="mb-0">Giá: {{ $val->price ? vdh_format_money($val->price) : 'N/A' }}</p>
-                                <p class="mb-0">Ngày đăng ký: {{ $val->created_at }}</p>
-                                <p class="mb-0">Xuất hóa đơn: {{ $val->is_vat == 1 ? 'Có' : 'Không' }}</p>
-                            </td>
-                            <td>
-                                <p class="mb-0">Họ tên: <b>{{ $val->fullname ?? null }}</b></p>
-                                <p class="mb-0">Email: {{ $val->email ?? null }}</p>
-                                <p class="mb-0">Số điện thoại: {{ $val->phone ?? null }}</p>
-                                <p class="mb-0">CCCD: {{ $val->cccd ?? null }}</p>
-                                <p class="mb-0">Năm sinh: {{ $val->birthday ?? null }}</p>
-                                <p class="mb-0">Đơn vị: {{ $val->company ?? null }}</p>
-                            </td>
-                            <td>
-                                <a href="{{ viewImage($val->cccd_front) }}" data-fancybox="gallery">
-                                    <img src="{{ viewImage($val->cccd_front) }}" alt="CCCD mặt trước" width="60px" class="rounded">
-                                </a>
-                                
-                                <a href="{{ viewImage($val->cccd_back) }}" data-fancybox="gallery">
-                                    <img src="{{ viewImage($val->cccd_back) }}" alt="CCCD mặt sau" width="60px" class="rounded">
-                                </a>
-                                
-                                <a href="{{ viewImage($val->degree) }}" data-fancybox="gallery">
-                                    <img src="{{ viewImage($val->degree) }}" alt="Bằng cấp" width="60px" class="rounded">
-                                </a>
+                                    <p class="mb-0">
+                                        <a href="#" data-type="select" class="status" data-name="status_id" data-pk="{{ $val->id ?? null }}" data-url="{{ route('order.update') }}" data-title="Trạng thái" data-value="{{ $val->status_id }}">{{ $val->status->name ?? 'Trạng thái' }}</a>
+                                    </p>
+                                </td>
+                                <td>
+                                    <p class="mb-0">Tên: <b>{{ $val->course->title ?? null }}</b></p>
+                                    <p class="mb-0">Lớp: {{ $val->class_code ?? null }}</p>
+                                    <p class="mb-0">Khai giảng: {{ $val->start_date ?? null }}</p>
+                                    <p class="mb-0">Giá: {{ $val->price ? vdh_format_money($val->price) : 'N/A' }}</p>
+                                    <p class="mb-0">Ngày đăng ký: {{ $val->created_at }}</p>
+                                    <p class="mb-0">Xuất hóa đơn: {{ $val->is_vat == 1 ? 'Có' : 'Không' }}</p>
+                                </td>
+                                <td>
+                                    <p class="mb-0">Họ tên: <b>{{ $val->fullname ?? null }}</b></p>
+                                    <p class="mb-0">Email: {{ $val->email ?? null }}</p>
+                                    <p class="mb-0">Số điện thoại: {{ $val->phone ?? null }}</p>
+                                    <p class="mb-0">CCCD: {{ $val->cccd ?? null }}</p>
+                                    <p class="mb-0">Năm sinh: {{ $val->birthday ?? null }}</p>
+                                    <p class="mb-0">Đơn vị: {{ $val->company ?? null }}</p>
+                                </td>
+                                <td>
+                                    <a href="{{ viewImage($val->cccd_front) }}" data-fancybox="gallery">
+                                        <img src="{{ viewImage($val->cccd_front) }}" alt="CCCD mặt trước" width="60px" class="rounded">
+                                    </a>
+                                    
+                                    <a href="{{ viewImage($val->cccd_back) }}" data-fancybox="gallery">
+                                        <img src="{{ viewImage($val->cccd_back) }}" alt="CCCD mặt sau" width="60px" class="rounded">
+                                    </a>
+                                    
+                                    <a href="{{ viewImage($val->degree) }}" data-fancybox="gallery">
+                                        <img src="{{ viewImage($val->degree) }}" alt="Bằng cấp" width="60px" class="rounded">
+                                    </a>
 
-                                <a href="{{ viewImage($val->signature) }}" data-fancybox="gallery">
-                                    <img src="{{ viewImage($val->signature) }}" alt="Chữ ký" width="60px" class="rounded">
-                                </a>
-                            </td>
-                            <td>
-                                {{ vdh_label_button(route('order.create', ['id' => $val->id]), route('order.delete', ['id' => $val->id])) }}
-                                <div class="mt-2">
-                                    <a href="{{ route('order.pdf', ['id' => $val->id]) }}" class="btn btn-sm btn-outline-warning">Tạo phiếu đăng ký</a>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                @endif
-            </tbody>
-        </table>
+                                    <a href="{{ viewImage($val->signature) }}" data-fancybox="gallery">
+                                        <img src="{{ viewImage($val->signature) }}" alt="Chữ ký" width="60px" class="rounded">
+                                    </a>
+                                </td>
+                                <td>
+                                    {{ vdh_label_button(route('order.create', ['id' => $val->id]), route('order.delete', ['id' => $val->id])) }}
+                                    <div class="mt-2">
+                                        <a href="{{ route('order.pdf', ['id' => $val->id]) }}" class="btn btn-sm btn-outline-warning">Tạo phiếu đăng ký</a>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endif
+                </tbody>
+            </table>
+        </div>
+        
         {{ !empty($orders) ? vdh_paginate($orders, $orders->total()) : null }}
     </div>
 </div>
